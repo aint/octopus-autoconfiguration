54eaeadf49f0d6e40650bfb914e9ce3992e84f00
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2018-12-05 21:22:14 +0200

    Introduce new JSON model

--- src/main/java/com/github/aint/octopus/DependencyJson.java
+++ src/main/java/com/github/aint/octopus/DependencyJson.java
@@ -1,20 +1,31 @@
 package com.github.aint.octopus;
 
+import java.util.Map;
+import java.util.Set;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DependencyJson {
 
-    private String eventType;
+    private EventType eventType;
 
-    private String name;
+    private String serviceName;
 
-    private Map<String, String> dependencies; // use enum with type
+    private Map<DependencyType, Set<String>> dependencies;
+
+    public enum EventType {
+        CREATE, DELETE
+    }
+
+    public enum DependencyType {
+        SERVICES, DATABASES, THIRD_PARTY
+    }
 
 }
