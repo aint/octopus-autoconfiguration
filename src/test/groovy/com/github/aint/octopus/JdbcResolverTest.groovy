9cc90d21becd41a4d17156856ae31ff88c0a9711
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-01-11 22:51:26 +0200

    Add integration test for getting db names

--- src/test/groovy/com/github/aint/octopus/JdbcResolverTest.groovy
+++ src/test/groovy/com/github/aint/octopus/JdbcResolverTest.groovy
@@ -3,6 +3,10 @@ package com.github.aint.octopus
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.sql.Driver
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class JdbcResolverTest extends Specification {
 
     @Unroll
@@ -17,5 +21,39 @@ class JdbcResolverTest extends Specification {
         "com.mysql.cj.jdbc.Driver"        || "MySQL"
         ""                                || "Unknown DB"
     }
+
+    @Unroll
+    def "getDbNames jdbc drivers #driverName"() {
+        given:
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver d = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(d);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        and:
+        DriverManager.registerDriver(Class.forName(driverName).newInstance() as Driver)
+
+        when:
+        def dbNames = JdbcResolver.getDbNames()
+
+        then:
+        dbNames.size() == 1
+
+        and:
+        dbNames[0] == dbName
+
+        where:
+        driverName                                     || dbName
+        "org.h2.Driver"                                || "Unknown DB"
+        "org.postgresql.Driver"                        || "PostgreSql"
+        "com.mysql.cj.jdbc.Driver"                     || "MySQL"
+        "com.microsoft.sqlserver.jdbc.SQLServerDriver" || "SQLServer"
+    }
+
 }
 
