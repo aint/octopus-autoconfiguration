36bc484abedcd2575337761e6b606492123e011f
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-01-15 21:11:52 +0200

    Introduce integration service

---
+++ src/main/java/com/github/aint/octopus/IntegrationService.java
@@ -0,0 +1,37 @@
+package com.github.aint.octopus;
+
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IntegrationService {
+
+    private final Set<String> propertyNames;
+    private final String integrationPropertyPrefix;
+
+    @Autowired
+    public IntegrationService(ApplicationPropertiesProvider propertiesProvider, String integrationPropertyPrefix) {
+        propertyNames = propertiesProvider.getPropertyNames();
+        this.integrationPropertyPrefix = integrationPropertyPrefix;
+    }
+
+    public Set<String> getServiceNames() {
+        String servicesEntity = ".services";
+        UnaryOperator<String> parseFn = str -> str.substring(integrationPropertyPrefix.length() + servicesEntity.length() + 1);
+        return parseEntityNames(propertyNames, integrationPropertyPrefix, servicesEntity, parseFn);
+    }
+
+    private Set<String> parseEntityNames(Set<String> strings,
+                                         String integrationPrefix,
+                                         String entity,
+                                         Function<String, String> parseFn) {
+        return strings.stream()
+                .filter(s -> s.startsWith(integrationPrefix + entity))
+                .map(parseFn)
+                .collect(Collectors.toSet());
+    }
+
+}
