b8e833338652a93f57ded32ceb51399fa6cd5ba0
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-01-16 23:48:43 +0200

    Cleanup actuator endpoint

--- src/main/java/com/github/aint/octopus/IntegrationsActuatorEndpoint.java
+++ src/main/java/com/github/aint/octopus/IntegrationsActuatorEndpoint.java
@@ -1,17 +1,17 @@
 package com.github.aint.octopus;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.core.SpringVersion;
-
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.*;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Endpoint(id = "integrations")
@@ -20,52 +20,27 @@ public class IntegrationsActuatorEndpoint {
     @Autowired
     private ApplicationPropertiesProvider propertiesProvider;
 
+    @Autowired
+    private IntegrationService integrationService;
+
     @ReadOperation
     public DependencyJson integrations() {
+
+
+
         Set<String> strings = propertiesProvider.getPropertyNames();
 
-        String integrationPrefix = getPropertyValue(strings,"octopus.integration.prefix").orElse("integration.base-url");
-
-        String servicesEntity = ".services";
-        Set<String> services = parseEntityNames(strings, integrationPrefix, servicesEntity,
-                (String s) -> s.substring(integrationPrefix.length() + servicesEntity.length() + 1));
-
         Map<DependencyJson.DependencyType, Set<String>> deps = new EnumMap<>(DependencyJson.DependencyType.class);
-        deps.put(DependencyJson.DependencyType.SERVICES, services);
-
-        String lambdasEntity = ".lambdas";
-        UnaryOperator<String> lambdaParserFn = (String s) -> {
-            int beginIndex = integrationPrefix.length() + ".lambdas".length() + 1;
-            int endIndex = s.length() - 5;
-            return s.substring(beginIndex, endIndex);
-        };
-        Set<String> lambdas = parseEntityNames(strings, integrationPrefix, lambdasEntity, lambdaParserFn);
-
-        deps.put(DependencyJson.DependencyType.LAMBDAS, lambdas);
-
-        String thirdPartyEntity = ".third-party";
-        Set<String> thirdParty = parseEntityNames(strings, integrationPrefix, thirdPartyEntity,
-                (String s) -> s.substring(integrationPrefix.length() + thirdPartyEntity.length() + 1));
-
-        deps.put(DependencyJson.DependencyType.THIRD_PARTY, thirdParty);
+        deps.put(DependencyJson.DependencyType.SERVICES, integrationService.getServiceNames());
+        deps.put(DependencyJson.DependencyType.LAMBDAS, integrationService.getLambdaNames());
+        deps.put(DependencyJson.DependencyType.THIRD_PARTY, integrationService.getThirdPartyNames());
+        deps.put(DependencyJson.DependencyType.DATABASES, JdbcResolver.getDbNames());
 
         String serviceName = getPropertyValue(strings, "application.name")
                 .orElseThrow(() -> new NoSuchElementException("application.name property not found"));
 
-        deps.put(DependencyJson.DependencyType.DATABASES, JdbcResolver.getDbNames());
-
         String serviceMetadata = String.format("%s %s", getJavaVersion(), getSpringVersion());
         return new DependencyJson(DependencyJson.EventType.CREATE, serviceName, serviceMetadata, deps);
-    }
-
-    private Set<String> parseEntityNames(Set<String> strings,
-                                         String integrationPrefix,
-                                         String entity,
-                                         Function<String, String> parseFn) {
-        return strings.stream()
-                .filter(s -> s.startsWith(integrationPrefix + entity))
-                .map(parseFn)
-                .collect(Collectors.toSet());
     }
 
     private Optional<String> getPropertyValue(Set<String> properties, String propertyName) {
