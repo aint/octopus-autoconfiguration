5d18a81f65250876cb2bed61c60f6303590587b7
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-01-17 23:23:22 +0200

    Cleanup integration endpoint

--- src/main/java/com/github/aint/octopus/IntegrationsActuatorEndpoint.java
+++ src/main/java/com/github/aint/octopus/IntegrationsActuatorEndpoint.java
@@ -2,8 +2,6 @@ package com.github.aint.octopus;
 
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,52 +9,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
-import org.springframework.core.SpringVersion;
 
 @Slf4j
 @Endpoint(id = "integrations")
 public class IntegrationsActuatorEndpoint {
 
     @Autowired
-    private ApplicationPropertiesProvider propertiesProvider;
+    private IntegrationService integrationService;
 
     @Autowired
-    private IntegrationService integrationService;
+    private SpringApplicationMetadata springApplicationMetadata;
 
     @ReadOperation
     public DependencyJson integrations() {
-
-
-
-        Set<String> strings = propertiesProvider.getPropertyNames();
-
         Map<DependencyJson.DependencyType, Set<String>> deps = new EnumMap<>(DependencyJson.DependencyType.class);
         deps.put(DependencyJson.DependencyType.SERVICES, integrationService.getServiceNames());
         deps.put(DependencyJson.DependencyType.LAMBDAS, integrationService.getLambdaNames());
         deps.put(DependencyJson.DependencyType.THIRD_PARTY, integrationService.getThirdPartyNames());
         deps.put(DependencyJson.DependencyType.DATABASES, JdbcResolver.getDbNames());
 
-        String serviceName = getPropertyValue(strings, "application.name")
-                .orElseThrow(() -> new NoSuchElementException("application.name property not found"));
-
-        String serviceMetadata = String.format("%s %s", getJavaVersion(), getSpringVersion());
-        return new DependencyJson(DependencyJson.EventType.CREATE, serviceName, serviceMetadata, deps);
-    }
-
-    private Optional<String> getPropertyValue(Set<String> properties, String propertyName) {
-        return properties.stream()
-                .filter(s -> s.equals(propertyName))
-                .map(s -> propertiesProvider.getProperty(s))
-                .findFirst();
-    }
-
-
-    private static String getJavaVersion() {
-        return System.getProperty("java.version");
-    }
-
-    private static String getSpringVersion() {
-        return SpringVersion.getVersion();
+        String appName = springApplicationMetadata.getApplicationName();
+        String appMetadata = springApplicationMetadata.getApplicationMetadata();
+        return new DependencyJson(DependencyJson.EventType.CREATE, appName, appMetadata, deps);
     }
 
 }
