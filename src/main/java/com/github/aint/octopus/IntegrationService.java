1080cb1bd6a89498d60ac20692d1729a7fcfa7c7
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-02-22 00:19:46 +0200

    Refactor integration service

--- src/main/java/com/github/aint/octopus/IntegrationService.java
+++ src/main/java/com/github/aint/octopus/IntegrationService.java
@@ -1,8 +1,6 @@
 package com.github.aint.octopus;
 
 import java.util.Set;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class IntegrationService {
 
-    private final Pattern pattern;
     private final Set<String> propertyNames;
     private final String integrationPropertyPrefix;
 
@@ -19,42 +16,27 @@ public class IntegrationService {
     public IntegrationService(ApplicationPropertiesProvider propertiesProvider, String integrationPropertyPrefix) {
         propertyNames = propertiesProvider.getPropertyNames();
         this.integrationPropertyPrefix = integrationPropertyPrefix;
-        this.pattern = Pattern.compile(integrationPropertyPrefix + "\\.services\\.(.+)\\..+");
     }
 
     public Set<String> getServiceNames() {
-        String servicesEntity = ".services";
-        UnaryOperator<String> parseFn = (String s) -> {
-            Matcher m = pattern.matcher(s);
-            return m.matches() ? m.group(1) : null;
-        };
-        return parseEntityNames(propertyNames, integrationPropertyPrefix, servicesEntity, parseFn);
+        return parseEntityNames("services");
     }
 
     public Set<String> getLambdaNames() {
-        String lambdasEntity = ".lambdas";
-        UnaryOperator<String> parseFn = (String s) -> {
-            int beginIndex = integrationPropertyPrefix.length() + lambdasEntity.length() + 1;
-            int tailSize = s.endsWith("name") ? ".name".length() : ".qualifier".length();
-            int endIndex = s.length() - tailSize;
-            return s.substring(beginIndex, endIndex);
-        };
-        return parseEntityNames(propertyNames, integrationPropertyPrefix, lambdasEntity, parseFn);
+        return parseEntityNames("lambdas");
     }
 
     public Set<String> getThirdPartyNames() {
-        String thirdPartyEntity = ".third-party";
-        UnaryOperator<String> parseFn = str -> str.substring(integrationPropertyPrefix.length() + thirdPartyEntity.length() + 1);
-        return parseEntityNames(propertyNames, integrationPropertyPrefix, thirdPartyEntity, parseFn);
+        return parseEntityNames("third-party");
     }
 
-    private Set<String> parseEntityNames(Set<String> strings,
-                                         String integrationPrefix,
-                                         String entity,
-                                         Function<String, String> parseFn) {
-        return strings.stream()
-                .filter(s -> s.startsWith(integrationPrefix + entity))
-                .map(parseFn)
+    private Set<String> parseEntityNames(String entity) {
+        Pattern pattern = Pattern.compile(integrationPropertyPrefix + "\\." + entity + "\\.(.+)\\..+");
+
+        return propertyNames.stream()
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .map(m -> m.group(1))
                 .collect(Collectors.toSet());
     }
 
