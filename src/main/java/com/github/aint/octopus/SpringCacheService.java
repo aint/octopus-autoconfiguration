5cfc61ff17fafce916dc694abfb3a85063fa63a4
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-02-27 23:09:59 +0200

    Refactor spring cache service

--- src/main/java/com/github/aint/octopus/SpringCacheService.java
+++ src/main/java/com/github/aint/octopus/SpringCacheService.java
@@ -1,39 +1,41 @@
 package com.github.aint.octopus;
 
-import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
-//@ConditionalOnBean(CacheManager.class)
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SpringCacheService {
 
-    private final ObjectProvider<CacheManager> optionalCacheManager;
-//    private final ObjectProvider<CacheProperties> optionalCacheProperties;
+    private final CacheManager cacheManager;
+    private final CacheProperties cacheProperties;
 
-    public Optional<String> getCacheProviderName() {
-        return Optional.ofNullable(optionalCacheManager.getIfAvailable())
-                .map(c -> c.getClass().getSimpleName())
-                .map(c -> {
-                    if (c.toLowerCase().contains("memcached")) {
-                        System.out.println("It's Memcached");
-                        return "Memcached";
-                    } else if (c.toLowerCase().contains("redis")) {
-                        System.out.println("It's Redis");
-                        return "Redis";
-                    } else if (c.toLowerCase().contains("hazelcast")) {
-                        System.out.println("It's Hazelcast");
-                        return "Hazelcast";
-                    }
+    public String getCacheProviderName() {
+        if (cacheProperties.getType() != null) {
+            String cacheProvider = cacheProperties.getType().name();
+            log.debug("{} detected", cacheProvider);
+            return cacheProvider;
+        }
 
-                    System.out.println(c);
-                    return c;
-                });
+        String cacheProvider = cacheManager.getClass().getSimpleName().toLowerCase();
+        if (cacheProvider.contains("memcached")) {
+            log.debug("Memcached detected");
+            return "Memcached";
+        } else if (cacheProvider.contains("redis")) {
+            log.debug("Redis detected");
+            return "Redis";
+        } else if (cacheProvider.contains("hazelcast")) {
+            log.debug("Hazelcast detected");
+            return "Hazelcast";
+        }
+
+        log.debug("{} detected", cacheProvider);
+        return cacheProvider;
     }
 
 }
