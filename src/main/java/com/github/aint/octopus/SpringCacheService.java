ad119637e45f23152fda9e7951bf2d789040adc4
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-02 23:45:14 +0200

    Sprinng cache service resolves cache name and type

--- src/main/java/com/github/aint/octopus/SpringCacheService.java
+++ src/main/java/com/github/aint/octopus/SpringCacheService.java
@@ -1,9 +1,12 @@
 package com.github.aint.octopus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.cache.CacheType;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
@@ -15,27 +18,45 @@ public class SpringCacheService {
     private final CacheManager cacheManager;
     private final CacheProperties cacheProperties;
 
-    public String getCacheProviderName() {
+    public CacheProvider getCacheProvider() {
         if (cacheProperties.getType() != null) {
             String cacheProvider = cacheProperties.getType().name();
             log.debug("{} detected", cacheProvider);
-            return cacheProvider;
+            return new CacheProvider(cacheProvider, isEmbeddedType(cacheProvider) ? "Embedded" : "Standalone");
         }
 
         String cacheProvider = cacheManager.getClass().getSimpleName();
         if (cacheProvider.toLowerCase().contains("memcached")) {
             log.debug("Memcached detected");
-            return "Memcached";
+            return new CacheProvider("Memcached", "Standalone");
         } else if (cacheProvider.toLowerCase().contains("redis")) {
             log.debug("Redis detected");
-            return "Redis";
+            return new CacheProvider("Redis", "Standalone");
         } else if (cacheProvider.toLowerCase().contains("hazelcast")) {
             log.debug("Hazelcast detected");
-            return "Hazelcast";
+            return new CacheProvider("Hazelcast", "Standalone");
         }
 
         log.debug("{} detected", cacheProvider);
-        return cacheProvider;
+        return new CacheProvider(cacheProvider, "Embedded");
+    }
+
+    private boolean isEmbeddedType(String cacheProviderName) {
+        if (cacheProviderName.contains(CacheType.CAFFEINE.name())
+                || cacheProviderName.contains(CacheType.GENERIC.name())
+                || cacheProviderName.contains(CacheType.SIMPLE.name())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class CacheProvider {
+
+        private String name;
+        private String type; // enum?
+
     }
 
 }
