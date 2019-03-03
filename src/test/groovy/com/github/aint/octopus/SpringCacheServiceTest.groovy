27826a29034d1b9fdd6f8dc8d76da7806ea02166
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-03 19:07:56 +0200

    Add more tests cases for cache service

--- src/test/groovy/com/github/aint/octopus/SpringCacheServiceTest.groovy
+++ src/test/groovy/com/github/aint/octopus/SpringCacheServiceTest.groovy
@@ -1,6 +1,6 @@
 package com.github.aint.octopus
 
-import groovy.transform.CompileStatic
+import io.sixhours.memcached.cache.MemcachedCacheManager
 import org.springframework.boot.autoconfigure.cache.CacheProperties
 import org.springframework.boot.autoconfigure.cache.CacheType
 import org.springframework.cache.support.SimpleCacheManager
@@ -9,13 +9,12 @@ import spock.lang.Subject
 
 import static org.assertj.core.api.Assertions.assertThat
 
-//@CompileStatic
 class SpringCacheServiceTest extends Specification {
 
     @Subject
     SpringCacheService springCacheService
 
-    def "getCacheProviderName"() {
+    def "getCacheProvider for simple cache"() {
         given:
         def cacheManager    = new SimpleCacheManager()
         def cacheProperties = new CacheProperties()
@@ -26,11 +25,26 @@ class SpringCacheServiceTest extends Specification {
         def cacheProvider = springCacheService.getCacheProvider()
 
         then:
-        assertThat(cacheProvider.getName()).isEqualTo(SimpleCacheManager.class.getSimpleName())
+        assertThat(cacheProvider.getName()).isEqualTo("Simple")
         assertThat(cacheProvider.getType()).isEqualTo("Embedded")
     }
 
-    def "getCacheProviderName with set cacheProperties type"() {
+    def "getCacheProvider for Memcached"() {
+        given:
+        def cacheManager    = new MemcachedCacheManager(null)
+        def cacheProperties = new CacheProperties()
+
+        springCacheService = new SpringCacheService(cacheManager, cacheProperties)
+
+        when:
+        def cacheProvider = springCacheService.getCacheProvider()
+
+        then:
+        assertThat(cacheProvider.getName()).isEqualTo("Memcached")
+        assertThat(cacheProvider.getType()).isEqualTo("Standalone")
+    }
+
+    def "getCacheProvider for Redis"() {
         given:
         def cacheManager    = null
         def cacheProperties = new CacheProperties()
