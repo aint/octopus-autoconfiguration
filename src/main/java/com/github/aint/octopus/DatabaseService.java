6841e2de4c50ca01a31c3096f6325de5c0064193
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-08 23:24:07 +0200

    Implement database service

---
+++ src/main/java/com/github/aint/octopus/DatabaseService.java
@@ -0,0 +1,36 @@
+package com.github.aint.octopus;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DatabaseService {
+
+    private final JdbcResolver jdbcResolver;
+    private final ObjectProvider<SpringCacheService> optionalSpringCacheService;
+
+    public Set<String> dbs() {
+        log.warn("DBS");
+        Set<String> dbs = new HashSet<>(jdbcResolver.getDbNames());
+        log.warn("DBS: jdbc {}", dbs);
+        optionalSpringCacheService.ifAvailable(springCacheService -> {
+            SpringCacheService.CacheProvider cacheProvider = springCacheService.getCacheProvider();
+            log.warn("DBS: cache {}", cacheProvider);
+            if ("Standalone".equals(cacheProvider.getType())) {
+                dbs.add(cacheProvider.getName());
+            }
+        });
+        return dbs;
+    }
+
+    // add NoSQL support
+
+}
