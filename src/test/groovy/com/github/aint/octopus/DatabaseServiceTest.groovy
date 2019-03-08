39a865fd290ae471f38edec1cafada7eaa0d797e
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-09 00:03:09 +0200

    Cover database service with tests

---
+++ src/test/groovy/com/github/aint/octopus/DatabaseServiceTest.groovy
@@ -0,0 +1,91 @@
+package com.github.aint.octopus
+
+import org.springframework.beans.factory.ObjectProvider
+import spock.lang.Specification
+import spock.lang.Subject
+
+import static org.assertj.core.api.Assertions.assertThat
+
+class DatabaseServiceTest extends Specification {
+
+    @Subject
+    private DatabaseService databaseService
+
+    private JdbcResolver jdbcResolver
+    private SpringCacheService springCacheService
+    private ObjectProvider<SpringCacheService> optionalSpringCacheService
+
+    def setup() {
+        jdbcResolver = Mock()
+        springCacheService = Mock()
+        optionalSpringCacheService = Spy()
+
+        databaseService = new DatabaseService(jdbcResolver, optionalSpringCacheService)
+    }
+
+    def "dbs with only jdbc"() {
+        given:
+        final def dbNames =  [ "MySQL" ]
+        jdbcResolver.dbNames >> dbNames
+
+        and:
+        optionalSpringCacheService.getIfAvailable() >> null
+
+        when:
+        def dbs = databaseService.dbs()
+
+        then:
+        assertThat(dbs).containsExactlyInAnyOrderElementsOf(dbNames)
+    }
+
+    def "dbs with only embedded cache"() {
+        given:
+        final def cacheProvider = new SpringCacheService.CacheProvider("Caffeine", "Embedded")
+        springCacheService.getCacheProvider() >> cacheProvider
+        optionalSpringCacheService.getIfAvailable() >> springCacheService
+
+        and:
+        jdbcResolver.dbNames >> []
+
+        when:
+        def dbs = databaseService.dbs()
+
+        then:
+        assertThat(dbs).isEmpty()
+    }
+
+    def "dbs with jdbc and embedded cache"() {
+        given:
+        final def dbNames =  [ "MySQL" ]
+        jdbcResolver.dbNames >> dbNames
+
+        and:
+        final def cacheProvider = new SpringCacheService.CacheProvider("Caffeine", "Embedded")
+        springCacheService.getCacheProvider() >> cacheProvider
+        optionalSpringCacheService.getIfAvailable() >> springCacheService
+
+        when:
+        def dbs = databaseService.dbs()
+
+        then:
+        assertThat(dbs).containsExactlyInAnyOrderElementsOf(dbNames)
+    }
+
+    def "dbs with jdbc and stadalone cache"() {
+        given:
+        final def dbNames =  [ "PostgreSQL" ]
+        jdbcResolver.dbNames >> dbNames
+
+        and:
+        final def cacheProvider = new SpringCacheService.CacheProvider("Redis", "Standalone")
+        springCacheService.getCacheProvider() >> cacheProvider
+        optionalSpringCacheService.getIfAvailable() >> springCacheService
+
+        when:
+        def dbs = databaseService.dbs()
+
+        then:
+        assertThat(dbs).containsExactlyInAnyOrderElementsOf(dbNames + cacheProvider.name)
+    }
+
+}
