d455b6c079b74f29cc33793ee3f87d498b352483
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-11 23:26:34 +0200

    Replace pure java http request with restTemplate

--- src/main/java/com/github/aint/octopus/ApplicationListenerBean.java
+++ src/main/java/com/github/aint/octopus/ApplicationListenerBean.java
@@ -1,25 +1,21 @@
 package com.github.aint.octopus;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStoppedEvent;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.springframework.web.client.RestTemplate;
 
 public class ApplicationListenerBean implements ApplicationListener {
 
     private final String octopusUrl;
     private final OctopusService octopusService;
+    private final RestTemplate restTemplate;
 
-    public ApplicationListenerBean(String octopusUrl, OctopusService octopusService) {
+    public ApplicationListenerBean(String octopusUrl, OctopusService octopusService, RestTemplate restTemplate) {
         this.octopusUrl = octopusUrl;
         this.octopusService = octopusService;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -34,24 +30,7 @@ public class ApplicationListenerBean implements ApplicationListener {
     }
 
     private void sendPostRequest(DependencyJson json) {
-        try {
-            URL url = new URL(octopusUrl);
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod("POST");
-            http.setDoOutput(true);
-
-            byte[] out = new ObjectMapper().writeValueAsBytes(json);
-            int length = out.length;
-
-            http.setFixedLengthStreamingMode(length);
-            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            http.connect();
-            try (OutputStream os = http.getOutputStream()) {
-                os.write(out);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        restTemplate.postForEntity(octopusUrl, json, Void.class);
     }
 
 }
