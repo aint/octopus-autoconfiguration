663b9e43787ee433336814b87f58453127d529cf
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-03-16 23:40:28 +0200

    Create cache service in autoconfiguration

--- src/main/java/com/github/aint/octopus/OctopusAutoConfiguration.java
+++ src/main/java/com/github/aint/octopus/OctopusAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.github.aint.octopus;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @ComponentScan(basePackageClasses = OctopusAutoConfiguration.class)
 public class OctopusAutoConfiguration {
+
+    @Bean
+    @ConditionalOnBean(CacheManager.class)
+    public SpringCacheService springCacheService(CacheManager cacheManager) {
+        return new SpringCacheService(cacheManager);
+    }
 
     @Bean
     public IntegrationService integrationService(ApplicationPropertiesProvider propertiesProvider) {
